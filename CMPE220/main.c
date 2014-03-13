4ef3332a12cd80b40544e8d95e9650b27ccc29b0
Huzefa <admin@Administrators-MacBook-Air.local>
2014-03-13 00:45:38 -0700

    Initial Commit

---
+++ CMPE220/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  CMPE220
+//
+//  Created by Huzefa on 3/13/14.
+//  Copyright (c) 2014 Huzefa. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
